d485817a6bd34ef556706cc1f2875b2df03e9b33
Dan Danilyuk and Mark Helt <dan+mark@>
2017-11-13 16:39:15 -0800

    link animals and regions

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,5 @@
 class Animal < ActiveRecord::Base
   has_many :sightings
+  has_many :regions, through: :sightings
   validates :species, :presence => true
 end
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,4 +1,5 @@
 class Region < ActiveRecord::Base
   has_many :sightings
+  has_many :animals, through: :sightings
   validates :description, :presence => true
 end
